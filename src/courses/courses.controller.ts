bdd6c610754c85ebef8b2bd20806503396ba8207
Joao Vitor Lima Lago Santos <vorque45@gmail.com>
2023-05-07 21:21:23 -0300

    Criação de controller

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CoursesService } from './courses.service';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
+
+@Controller('courses')
+export class CoursesControler {
+  constructor(private readonly coursesService: CoursesService) {}
+  @Get()
+  findAll() {
+    return this.coursesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coursesService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createCourseDto: CreateCourseDto) {
+    return this.coursesService.create(createCourseDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCouseDto: UpdateCourseDto) {
+    return this.coursesService.update(id, updateCouseDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.coursesService.remove(id);
+  }
+}
